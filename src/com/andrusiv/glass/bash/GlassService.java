6299b7134cb21cf9e650db29ca7ec6d4c08f6947
ostap.andrusiv <ostap.andrusiv@eleks.com>
2013-11-01 11:28:09 +0200

    updated service class

--- src/com/andrusiv/glass/bash/GlassService.java
+++ src/com/andrusiv/glass/bash/GlassService.java
@@ -5,23 +5,16 @@ import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
-import java.nio.charset.Charset;
 import java.util.UUID;
 import java.util.concurrent.Executors;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.HttpVersion;
 import org.apache.http.StatusLine;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.params.BasicHttpParams;
-import org.apache.http.params.HttpParams;
-import org.apache.http.params.HttpProtocolParams;
-import org.apache.http.util.EntityUtils;
 import org.json.JSONObject;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
@@ -47,7 +40,7 @@ public class GlassService extends Service {
 
 	private static final String BASH_URL = "http://api.ukrbash.org/1/pictures.getRandom.xml?limit=1&client=60e5ba6ea1e26a2c";
 	private static final String TRANSLATE_URL = "http://translate.google.com/translate_a/t?client=x&text={XXX}&hl=en&sl=uk&tl=en";
-	
+
 	public static final String SERVICE_BROADCAST = "com.andrusiv.glass.bash.RANDOM";
 	public static final String SERVICE_CARD = "home_card";
 	public static final String SERVICE_COMMAND = "command";
@@ -57,23 +50,24 @@ public class GlassService extends Service {
 	private static final String SERVICE_ITEM_URL = "intent_url";
 	private static final String SERVICE_ITEM_TEXT = "intent_text";
 
-
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startid) {
 		super.onStartCommand(intent, flags, startid);
-		Log.d(TAG, "GlassService onStartCommand() " + intent); 
+		Log.d(TAG, "GlassService onStartCommand() " + intent);
+		
 		GlassLocationManager.init(this);
-		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-		String homeCardId = preferences.getString(SERVICE_CARD, null);
+		String homeCardId = PreferenceManager.getDefaultSharedPreferences(this).getString(SERVICE_CARD, null);
+
 		TimelineHelper tlHelper = new TimelineHelper();
 		ContentResolver cr = getContentResolver();
 		if (homeCardId != null) {
 			// find and delete previous home card
-			TimelineItem timelineItem = tlHelper.queryTimelineItem(cr, homeCardId);
+			TimelineItem timelineItem = tlHelper.queryTimelineItem(cr,
+					homeCardId);
 			if (timelineItem != null && !timelineItem.getIsDeleted())
 				tlHelper.deleteTimelineItem(this, timelineItem);
 		}
-		
+
 		requestAndUpdateTimeline(cr, tlHelper);
 
 		return START_NOT_STICKY;
@@ -87,30 +81,39 @@ public class GlassService extends Service {
 				try {
 					picture = requestAndParse();
 				} catch (Exception e) {
-					Log.w(TAG, "Shit happened when requesting/parsing");
+					Log.w(TAG, "Something bad happened when requesting/parsing: " + e);
 				}
-				// create new home card
-				
-				String id = UUID.randomUUID().toString();
-				MenuItem delOption = MenuItem.newBuilder().setAction(MenuItem.Action.DELETE).build();
-				MenuItem readAloud = MenuItem.newBuilder().setAction(MenuItem.Action.READ_ALOUD).build();
-				MenuItem customOption = MenuItem.newBuilder()
-						.addValue(MenuValue.newBuilder().setDisplayName("Random Picture").build())
-						.setAction(MenuItem.Action.BROADCAST)
-						.setBroadcastAction(SERVICE_BROADCAST)
-						.build();
 
-				TimelineItem.Builder builder = tlHelper.createTimelineItemBuilder(GlassService.this, new SettingsSecure(cr));
-				TimelineItem item = builder.setId(id)
+				// create new home card
+				String id = UUID.randomUUID().toString();
+				MenuItem delOption = MenuItem.newBuilder()
+						.setAction(MenuItem.Action.DELETE).build();
+				MenuItem readAloud = MenuItem.newBuilder()
+						.setAction(MenuItem.Action.READ_ALOUD).build();
+//				MenuItem share = MenuItem.newBuilder()
+//						.setAction(MenuItem.Action.SHARE).build();
+				MenuItem customOption = MenuItem.newBuilder()
+						.addValue(MenuValue.newBuilder()
+								.setDisplayName("Random Picture")
+								.build())
+						.setAction(MenuItem.Action.BROADCAST)
+						.setBroadcastAction(SERVICE_BROADCAST).build();
+
+				TimelineItem.Builder builder = tlHelper
+						.createTimelineItemBuilder(GlassService.this, new SettingsSecure(cr));
+				TimelineItem item = builder
+						.setId(id)
 						.setHtml(picture.toHtml())
 						.setText(picture.translation)
 						.setIsPinned(true)
 						.addMenuItem(customOption)
 						.addMenuItem(readAloud)
+//						.addMenuItem(share)
 						.addMenuItem(delOption)
 						.build();
 
-				cr.insert(TimelineProvider.TIMELINE_URI, TimelineHelper.toContentValues(item));
+				cr.insert(TimelineProvider.TIMELINE_URI,
+						TimelineHelper.toContentValues(item));
 				SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(GlassService.this);
 				preferences.edit().putString(SERVICE_CARD, id).commit();
 			}
@@ -119,24 +122,27 @@ public class GlassService extends Service {
 	}
 
 	private UkrBashPicture requestAndParse() throws Exception {
-		HttpGet uri = new HttpGet(BASH_URL);    
+		HttpGet uri = new HttpGet(BASH_URL);
 
 		DefaultHttpClient client = new DefaultHttpClient();
 		HttpResponse resp = client.execute(uri);
 
 		StatusLine status = resp.getStatusLine();
 		if (status.getStatusCode() != 200) {
-		    Log.d(TAG, "HTTP error, invalid server status code: " + resp.getStatusLine());  
+			Log.d(TAG,
+					"HTTP error, invalid server status code: "
+							+ resp.getStatusLine());
 		}
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		Document doc = builder.parse(resp.getEntity().getContent());
-		
-		NodeList randomPicture = doc.getFirstChild().getFirstChild().getChildNodes();
-		
-		String imageUrl = ""; 
-		String itemText = ""; 
+
+		NodeList randomPicture = doc.getFirstChild().getFirstChild()
+				.getChildNodes();
+
+		String imageUrl = "";
+		String itemText = "";
 		for (int i = 0; i < randomPicture.getLength(); i++) {
 			Node item = randomPicture.item(i);
 			if ("title".equals(item.getNodeName())) {
@@ -146,62 +152,35 @@ public class GlassService extends Service {
 				imageUrl = item.getTextContent();
 			}
 		}
-		
+
 		String translation = "";
 		try {
-//			getTranslation(itemText);
-			translation = getTrNew(itemText);
+			translation = getTranslation(itemText);
 		} catch (Exception e) {
-			Log.w(TAG, "errors when getting translation for read aloud");
+			Log.w(TAG, "errors when getting translation for read aloud: " + e);
 		}
-		
+
 		return new UkrBashPicture(imageUrl, itemText, translation);
 	}
 
-	private static String getTrNew(String itemText) throws Exception {
+	private static String getTranslation(String itemText) throws Exception {
 		String from = "uk";
 		String to = "en";
 		String text = "http://translate.google.com/translate_a/t?"
-                + "client=o&text=" + URLEncoder.encode(itemText, "UTF-8")
-                + "&hl=en&sl=" + from + "&tl=" + to + "";
-        URL url = new URL(text);
-        URLConnection conn = url.openConnection();
-        // Look like faking the request coming from Web browser solve 403 error
-        conn.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
-        BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-        String json = in.readLine();
-        String trans = ((JSONObject)(new JSONObject(json).getJSONArray("sentences").get(0))).getString("trans");
-        byte[] bytes = json.getBytes("UTF-8");
-        in.close();
-        return trans;
-	}
-	
-	private static String getTranslation(String itemText) throws Exception {
-//		String query = URLEncoder.encode(itemText, "utf-8");
-		String url = TRANSLATE_URL.replace("{XXX}", itemText.replace(' ', '+')); ////query);
-		HttpGet uri = new HttpGet(url);    
-
-		HttpParams params = new BasicHttpParams();
-		HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
-		HttpProtocolParams.setContentCharset(params, "utf-8");
-		DefaultHttpClient client = new DefaultHttpClient(params);
-		
-		HttpResponse resp = client.execute(uri);
-		StatusLine status = resp.getStatusLine();
-		if (status.getStatusCode() != 200) {
-		    Log.d(TAG, "HTTP error, invalid server status code: " + resp.getStatusLine());  
-		}
-		HttpEntity entity = resp.getEntity();
-//		EntityUtils.getContentCharSet(entity);
-		byte[] byteEntity = EntityUtils.toByteArray(entity);
-		String result = new String(byteEntity);
-		String resultUtf8 = new String(byteEntity, Charset.forName("utf-8"));
-		String resultWin1251 = new String(byteEntity, Charset.forName("windows-1251"));
-		
-//		String resultUtf8 = EntityUtils.toString(entity, "utf-8");
-//		String resultWin1251 = EntityUtils.toString(entity, "windows-1251");
-
-		String trans = ((JSONObject)(new JSONObject(resultUtf8).getJSONArray("sentences").get(0))).getString("trans"); 
+				+ "client=o&text=" + URLEncoder.encode(itemText, "UTF-8")
+				+ "&hl=en&sl=" + from + "&tl=" + to + "";
+		URL url = new URL(text);
+		URLConnection conn = url.openConnection();
+		// Look like faking the request coming from Web browser solve 403 error
+		conn.setRequestProperty(
+				"User-Agent",
+				"Mozilla/5.0 (Windows; U; Windows NT 6.1; en-GB; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13 (.NET CLR 3.5.30729)");
+		BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
+		String json = in.readLine();
+		String trans = ((JSONObject) (new JSONObject(json)
+				.getJSONArray("sentences").get(0)))
+				.getString("trans");
+		in.close();
 		return trans;
 	}
 
